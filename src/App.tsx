fc80d3fa2e1918265ea12c44e229cde40fe8c708
RK-TruongDN <truongdn@rikkeisoft.com>
2021-12-19 22:38:39 +0700

    fix image footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,28 @@ import './stylesheet/App.css'
 import './stylesheet/tailwind.css'
 import { data, profile } from './data/data'
 import Header from './component/Header'
-
+import Footer from './assets/img/footer.png'
 
 function App() {
 
 	return (
 		<div className="App ml-10 mt-10 shadow-lg">
-			<Header name='Chú Hùng' time={20000} />
-			<div className="main"></div>
-			<div className="footer"></div>
+			<Header name='Truong Tom' time={31} />
+			<div className="main bg-gray-100 overflow-y-auto flex-grow flex-col overscroll-y-contain">
+				<div className="h-52 bg-white border"></div>
+				<div className="h-52 bg-white border"></div>
+				<div className="h-52 bg-white border"></div>
+				<div className="h-52 bg-white border"></div>
+				<div className="h-52 bg-white border"></div>
+				<div className="h-52 bg-white border"></div>
+				<div className="h-52 bg-white border"></div>
+				<div className="h-52 bg-white border"></div>
+				<div className="h-52 bg-white border"></div>
+				<div className="h-52 bg-white border"></div>
+			</div>
+			<div className="footer ">
+				<img src={Footer} alt="footer" className='h-[48px]' />
+			</div>
 		</div>
 	)
 }
