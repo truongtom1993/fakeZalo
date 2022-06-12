91fcbd2167dbc028d7cbae4b2c4743e55b505ccd
TruongDN <dang_nhat_truong@hotmail.com>
2022-06-12 22:50:21 +0700

    change default profile

--- src/slice/ProfileSlice.tsx
+++ src/slice/ProfileSlice.tsx
@@ -3,9 +3,11 @@ import { Profile } from '../interface/IMessage';
 const initProfile: Profile = {
 	userName: 'Người lạ',
 	status: 6 * 60,
-	avatarURL: 'https://i.postimg.cc/J0Yq1kR6/avatar.png',
+	avatarURL:
+		'https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
 	myName: 'Đặng Nhật Trường',
-	myAvatarUrl: 'https://i.postimg.cc/9Xb1jCQr/atphizyom.jpg',
+	myAvatarUrl:
+		'https://images.unsplash.com/photo-1545996124-0501ebae84d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
 };
 
 const profile: Profile = localStorage.getItem('profile') ? JSON.parse(localStorage.getItem('profile')) : initProfile;
